359677815038fb5308f44de8c82a8cc67589f789
Isaias Ogbe <ogbeisaias@gmail.com>
2026-03-28 08:56:58 -0400

    set view engine

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
     res.send("<h1>Home Page</h1>");
 
