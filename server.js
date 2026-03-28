2bf01b9822444d259f9464bdcaadaae60c02fc44
Isaias Ogbe <ogbeisaias@gmail.com>
2026-03-28 08:14:59 -0400

    added 3 routes

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get("/", (req, rest) => {
-    rest.send("Hello World");
+app.get("/", (req, res) => {
+    res.send("<h1>Home Page</h1>");
 
+});
+
+app.get("/form", (req, res) => {
+    res.send("<h1>Form Page</h1>");
+});
+
+app.get("/class1", (req, res) => {
+    res.send("<h1>Class 1 Page</h1>");
+});
+
+app.get("/class2", (req, res) => {
+    res.send("<h1>Class 2 Page</h1>");
 });
 
 app.listen(port, () => {
