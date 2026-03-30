db25803ba7d38043ab51f83046daeedcf142a033
Isaias Ogbe <ogbeisaias@gmail.com>
2026-03-30 14:55:17 -0400

    add database

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,27 @@
+import "dotenv/config";
+import cors from "cors";
 import express from "express";
 import type { Request, Response } from "express";
+import { Pool } from "pg";
+
 const app = express();
 const port = process.env.PORT || 8080;
 
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+
+const pool = new Pool({
+    host: PGHOST,
+    database: PGDATABASE,
+    user: PGUSER,
+    password: PGPASSWORD,
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+app.use(cors());
+app.use(express.json());
 app.set("view engine", "ejs");
 
 
@@ -20,8 +39,17 @@ app.get("/about", (req: Request, res: Response) => {
 });
 
 app.route("/class")
-    .get((req: Request, res: Response) => {
-    res.render("pages/class");
+    .get( async (req: Request, res: Response) => {
+        const client = await pool.connect();
+        try {
+            const result = await client.query("SELECT first_name, last_name, email, class_type FROM students");
+            console.log(result.rows);
+            res.render("pages/class", { students: result.rows });
+        } catch (err) {
+            console.error(err);
+        } finally {
+            client.release();
+        }
     })
     .post((req: Request, res: Response) => {
         res.render("pages/class");
